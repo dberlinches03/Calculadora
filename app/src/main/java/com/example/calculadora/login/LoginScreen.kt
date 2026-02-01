2e65c9c05e2ebb358d43b171af1cca025ff1f0c4
dberl <dberlinches2003@gmail.com>
2026-02-01 13:47:04 +0100

    Creada la pantalla de LoginScreen.kt

---
+++ app/src/main/java/com/example/calculadora/login/LoginScreen.kt
@@ -0,0 +1,26 @@
+package com.example.calculadora.login
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun LoginScreen(onLoginSuccess: () -> Unit) {
+    Column(
+        modifier = Modifier.padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Iniciar sesión", style = MaterialTheme.typography.headlineMedium)
+
+        Button(onClick = { onLoginSuccess() }) {
+            Text("Acceder con Google")
+        }
+    }
+}
